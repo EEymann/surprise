623d31d6552aa2ba0f9d7fe192c79554a4201acf
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-10-02 11:34:23 -0700

    convert the showSurprise function to an anonymous function.

--- surprise.js
+++ surprise.js
@@ -1,9 +1,9 @@
 const surpriseSection = document.getElementById('surprise');
 
-function showSurprise() {
-    surpriseSection.textContent = '🎉 Surprise! 🎉';
-}
+
 
 const randomTime = Math.random() * 4000;
 
-setTimeout(showSurprise, randomTime);
+setTimeout(function () {
+surpriseSection.textContent = '🎉 Surprise! 🎉';
+}, randomTime);
